02f940a31d651ce7ff9abaf2ace28aa4d0eebd45
kushagra-successive <kushagra.bajpai@successive.tech>
2024-04-03 21:42:40 +0530

    Updated Changes

--- src/Assignment1/UserCard.jsx
+++ src/Assignment1/UserCard.jsx
@@ -3,11 +3,11 @@ import styles from "./assignment.module.css";
 const UserCard = (props) => {
   return (
     <>
-      {props.info.map((element) => (
+      {props.info.map(({ name, age, avatarURL }) => (
         <div className={styles.card_style}>
-          <h1>{element.name}</h1>
-          <img src={element.avatarURL} alt="avatar" />
-          <h2>{element.age}</h2>
+          <h1>{name}</h1>
+          <img src={avatarURL} alt="avatar" />
+          <h2>{age}</h2>
         </div>
       ))}
     </>
