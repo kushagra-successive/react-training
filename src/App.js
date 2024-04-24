d3a531677cf1145036ea41b4735cf70ea1dc6dd6
kushagra-successive <kushagra.bajpai@successive.tech>
2024-04-24 12:19:40 +0530

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-// import React from "react";
-// import Assignment1 from "./Assignment1/Assignment_1";
-// import Wrap from "./Assignment2/Wrap";
-// import WrapAssignment3 from "./Assignment3/WrapAssignment3";
 import WrapAssignment5 from "./Assignment5/WrapAssignment5";
 import "./App.css";
 
 function App() {
   return (
     <>
-      {/* <h1>Assignment1</h1>
-      <hr />
-      <Assignment1 />
-      <h1>Assignment2</h1>
-      <hr />
-      <Wrap /> */}
-      {/* <h1>Assignment3</h1>
-      <hr />
-      <WrapAssignment3 /> */}
+      <h1>Assignment5</h1>
       <WrapAssignment5 />
-      <hr />
     </>
   );
 }
