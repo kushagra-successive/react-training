89363c4ca822512704c5bf67eaeb5a99817ba9eb
kushagra-successive <kushagra.bajpai@successive.tech>
2024-04-25 18:23:17 +0530

    code clean up

--- src/Assignment6/question2/AboutPageDetails.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-const AboutPageDetails = () => {
-  const { aboutId } = useParams();
-  return <div>HELLO ! {aboutId}</div>;
-};
-
-export default AboutPageDetails;
